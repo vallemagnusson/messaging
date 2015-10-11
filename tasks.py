5f101f43b510903b618a005b27513d5974b06e2c
vallemagnusson <valle.magnusson@gmail.com>
2015-10-11 23:35:24 +0200

    hatar dig

--- tasks.py
+++ tasks.py
@@ -33,10 +33,9 @@ def getTweets():
 				print "object found"
 				if obj["name"] == "tweets_19.txt":
 					(response, tweet_file) = conn.get_object(bucket['name'],obj["name"])
-					new_file = open("tweets.txt", "wb")
+					new_file = open("tweets.txt", "w")
 					new_file.write(tweet_file)
 					new_file.close()
-					tweet_file.write()
 					print "start read file"
 					#print tweet_file
 					dictionary_temp = Counter(readJSON(new_file))
@@ -54,7 +53,7 @@ def readJSON(tweet_file):
 	JSONFile = open(tweet_file, "r")
 	tweet_count = 0
 	dictionary = {"han": 0, "hon": 0, "den": 0, "det": 0, "denna": 0, "denne": 0, "hen": 0}
-	print "tweet_count set and dictionary"
+	#print "tweet_count set and dictionary"
 	#for line in JSONFile:
 	for line in new_file:
 		try:

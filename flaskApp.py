e2886f7f463850e24fe1492bc128256d6e92e75a
vallemagnusson <valle.magnusson@gmail.com>
2015-10-13 14:15:14 +0200

    a

--- flaskApp.py
+++ flaskApp.py
@@ -1,10 +1,12 @@
 #!flask/bin/python
 from flask import Flask
 from tasks import getTweets
+import sys
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def start():
 	#return "hello world", 200
 	primes = getTweets.delay()
@@ -15,4 +17,3 @@ def start():
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
-

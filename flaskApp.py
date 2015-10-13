350d0c6a773ba4e1e3c5b448b7a31a9624d25b7e
vallemagnusson <valle.magnusson@gmail.com>
2015-10-13 21:18:32 +0200

    asas

--- flaskApp.py
+++ flaskApp.py
@@ -1,15 +1,13 @@
 #!flask/bin/python
-from celery import Celery
+
 from flask import Flask
 import subprocess
 import sys
 import os
-import swiftclient.client
-import time
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/") #, methods=['GET'])
 def start():
 	#return "hello world", 200
 	print "start"

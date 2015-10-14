6ba9b4efb300a10235af2dbedc4838ae9065d613
vallemagnusson <valle.magnusson@gmail.com>
2015-10-14 17:29:12 +0200

    sal

--- flaskApp.py
+++ flaskApp.py
@@ -13,11 +13,6 @@ from collections import Counter
 #appC = Celery('tasks', backend='amqp', broker='amqp://')
 app = Flask(__name__)
 
-config = {'user':os.environ['OS_USERNAME'], 
-          'key':os.environ['OS_PASSWORD'],
-          'tenant_name':os.environ['OS_TENANT_NAME'],
-          'authurl':os.environ['OS_AUTH_URL']}
-
 @app.route("/messaging", methods=['GET'])
 def start():
 	print "start"

ad7d5bf6cee1c29ba5ea4ec8490e7b651a73d501
vallemagnusson <valle.magnusson@gmail.com>
2015-10-14 14:18:07 +0200

    baba

--- flaskApp.py
+++ flaskApp.py
@@ -18,19 +18,22 @@ def start():
 	print "start"
 	print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 	#primes = 1
-	test = getTweets.s()
+	test = getTweets()
 	primes = test.apply_async()
 	print "primes"
 	#print primes
 	print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-	#print primes.ready()
+	print primes.ready()
 	print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-	#while primes.ready() == False:
-	#	time.delay(5)
-	#print primes.get()
+	n = 0
+	while primes.ready() == False:
+		print "Waited " + str(n) + " seconds"
+		time.delay(1)
+		n += 1
+	return primes.get()
 	print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 	#return "hej pa dig"
-	return "hello world", 200
+	#return "hello world", 200
 
 
 if __name__ == "__main__":

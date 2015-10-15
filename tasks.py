4e8bc51cea98af1b4823d9f4da52ba982d7d358c
vallemagnusson <valle.magnusson@gmail.com>
2015-10-15 20:34:47 +0200

    kla

--- tasks.py
+++ tasks.py
@@ -10,7 +10,6 @@ from celery import Celery
 from collections import Counter
 import urllib2
 
-
 app = Celery('tasks', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.120:5672/app2')
 
 @app.task

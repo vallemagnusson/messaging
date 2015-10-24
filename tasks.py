27997a3c3948edfcf33bdd8608241e5a24455e06
vallemagnusson <valle.magnusson@gmail.com>
2015-10-24 12:24:35 +0200

    ujson

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 #!flask/bin/python
 
 import os
-import json
+import ujson
 import time
 #import swiftclient.client
 import sys
@@ -13,28 +13,26 @@ import urllib2
 app = Celery('tasks', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.120:5672/app2')
 
 @app.task
-def getTweets(tweetFileList):
+def getTweets(tweetFile):
 	dictionary_all = {}
-	for tweetFile in tweetFileList:
-		print "getTweets started with tweetfile: " + str(tweetFile)
-		urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + tweetFile)
-		urlResponse = urllib2.urlopen(urlRequest).read()
-		new_file = open(tweetFile, "w")
-		new_file.write(urlResponse)
-		new_file.close()
-		dictionary_all = readJSON(tweetFile)
-		os.remove(tweetFile)
+	print "getTweets started with tweetfile: " + str(tweetFile)
+	urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + tweetFile)
+	urlResponse = urllib2.urlopen(urlRequest).read()
+	new_file = open(tweetFile, "w")
+	new_file.write(urlResponse)
+	new_file.close()
+	dictionary_all = readJSON(tweetFile)
+	os.remove(tweetFile)
 	return dictionary_all
 
 @app.task
 def readJSON(tweet_file):
-	start_time = time.time()
 	JSONFile = open(tweet_file, "r")
 	tweet_count = 0
 	dictionary = {"han": 0, "hon": 0, "den": 0, "det": 0, "denna": 0, "denne": 0, "hen": 0}
 	for line in JSONFile:
 		try:
-			data = json.loads(line)
+			data = ujson.loads(line)
 			if data["retweet_count"] == 0:
 				tweet_count += 1
 				word_list = data["text"].lower().split()
@@ -43,6 +41,5 @@ def readJSON(tweet_file):
 						dictionary[i] += 1
 		except:
 			pass
-	stop_time = time.time()
 	dictionary.update({"tweet_count": tweet_count})
 	return dictionary

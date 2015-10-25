4f2f5f764b341696ae49e77875c684ed86dfc4a4
vallemagnusson <valle.magnusson@gmail.com>
2015-10-25 21:45:35 +0100

    många filer

--- test.py
+++ test.py
@@ -14,46 +14,48 @@ import urllib2
 #	new_file.write(urlResponse)
 #	new_file.close()
 ##### Create one larege file
-i = 0
-tweetFileList = []
-content = os.listdir("./")
-new_file = open("large_tweets.txt", "w")
-new_file.close()
-for x in content:
-	if "tweets" in x:
-		old_file = open(x, "r").read()
-		with open("large_tweets.txt", "a") as add_new_file:
-			add_new_file.write(old_file)
-#fileName = "tweets_2.txt"
-#fil = open(fileName, "r")
-##numb_line = sum(1 for _ in fil)
-#langd= len(fil.readlines()) -1
-#fil.close()
+#i = 0
+#tweetFileList = []
+#content = os.listdir("./")
+#new_file = open("large_tweets.txt", "w")
+#new_file.close()
+#for x in content:
+#	if "tweets" in x:
+#		old_file = open(x, "r").read()
+#		with open("large_tweets.txt", "a") as add_new_file:
+#			add_new_file.write(old_file)
 #
-##print numb_line
-##print langd
-#fil = open(fileName, "r")
-#filLines = fil.readlines()
-##print fil.readlines()
-##for x in range(numb_line):
-##	fileName = x % 100
-##
-##	new_file = open("new_tweet_file_" + str(fileName) + ".txt", "a")
-#count = 0
-##print filLines[0]
-#fileVar = 0
-#while langd != count:
-##for line in fil:
-#	#print line
-#	fileVar += 1
-#	count += 1
-#	if count > langd:
-#		break
-#	fileName = fileVar % 12
+#################################################
+fileName = "large_tweets.txt"
+fil = open(fileName, "r")
+#numb_line = sum(1 for _ in fil)
+langd= len(fil.readlines()) -1
+fil.close()
+
+#print numb_line
+#print langd
+fil = open(fileName, "r")
+filLines = fil.readlines()
+#print fil.readlines()
+#for x in range(numb_line):
+#	fileName = x % 100
+#
 #	new_file = open("new_tweet_file_" + str(fileName) + ".txt", "a")
-#	new_file.write(filLines[count])
-#	count += 1
-#	if count > langd:
-#		break
-#	new_file.write(filLines[count])
-#	#print line#
+count = 0
+#print filLines[0]
+fileVar = 0
+while langd != count:
+#for line in fil:
+	#print line
+	fileVar += 1
+	count += 1
+	if count > langd:
+		break
+	fileName = fileVar % 1000
+	new_file = open("new_tweet_file_" + str(fileName) + ".txt", "a")
+	new_file.write(filLines[count])
+	count += 1
+	if count > langd:
+		break
+	new_file.write(filLines[count])
+	#print line#

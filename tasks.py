acc00cd5c098969cf7a3e2a732b47ac63d86bd93
vallemagnusson <valle.magnusson@gmail.com>
2015-10-25 21:12:49 +0100

    igen

--- tasks.py
+++ tasks.py
@@ -14,9 +14,9 @@ app = Celery('tasks', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.1
 
 @app.task
 def getTweets(tweetFile):
-	#dictionary_all = {}
-	#print "getTweets started with tweetfile: " + str(tweetFile)
-	#start_file_time = time.time()
+	dictionary_all = {}
+	print "getTweets started with tweetfile: " + str(tweetFile)
+	start_file_time = time.time()
 	#urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + tweetFile)
 	#urlResponse = urllib2.urlopen(urlRequest).read()
 	#new_file = open(tweetFile, "w")

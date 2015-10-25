b4fc7040245eb079debca347199fde25512b6921
vallemagnusson <valle.magnusson@gmail.com>
2015-10-25 19:31:58 +0100

    make one large file

--- test.py
+++ test.py
@@ -2,25 +2,28 @@ import os
 
 import urllib2
 
+##### Download all files from container
 
-urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/")
-tweetFileList = urllib2.urlopen(urlRequest).read().split()
-print tweetFileList
-for tweetFile in tweetFileList:
-	urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + tweetFile)
-	urlResponse = urllib2.urlopen(urlRequest).read()
-	new_file = open(tweetFile, "w")
-	new_file.write(urlResponse)
-	new_file.close()
-##i = 0
-##tweetFileList = []
-##content = os.listdir("./")
-##new_file = open("large_tweets", "w")
-##new_file.close()
-##for x in content:
-##	old_file = open(x, "r").read()
-##	with open("large_tweets.txt", "a") as add_new_file:
-##		add_new_file.write(old_file)
+#urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/")
+#tweetFileList = urllib2.urlopen(urlRequest).read().split()
+#print tweetFileList
+#for tweetFile in tweetFileList:
+#	urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + tweetFile)
+#	urlResponse = urllib2.urlopen(urlRequest).read()
+#	new_file = open(tweetFile, "w")
+#	new_file.write(urlResponse)
+#	new_file.close()
+##### Create one larege file
+i = 0
+tweetFileList = []
+content = os.listdir("./")
+new_file = open("large_tweets", "w")
+new_file.close()
+for x in content:
+	if "tweets" in x:
+		old_file = open(x, "r").read()
+		with open("large_tweets.txt", "a") as add_new_file:
+			add_new_file.write(old_file)
 #fileName = "tweets_2.txt"
 #fil = open(fileName, "r")
 ##numb_line = sum(1 for _ in fil)

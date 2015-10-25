075cdb2b5119abbd9e55fae173e0802232e5e1c9
vallemagnusson <valle.magnusson@gmail.com>
2015-10-25 11:24:07 +0100

    time download and readJSON

--- tasks.py
+++ tasks.py
@@ -16,12 +16,17 @@ app = Celery('tasks', backend='amqp', broker='amqp://mava:orkarinte@130.238.29.1
 def getTweets(tweetFile):
 	dictionary_all = {}
 	print "getTweets started with tweetfile: " + str(tweetFile)
+	start_file_time = time.time()
 	urlRequest = urllib2.Request("http://smog.uppmax.uu.se:8080/swift/v1/tweets/" + tweetFile)
 	urlResponse = urllib2.urlopen(urlRequest).read()
 	new_file = open(tweetFile, "w")
 	new_file.write(urlResponse)
 	new_file.close()
+	start_readJSON_time = time.time()
 	dictionary_all = readJSON(tweetFile)
+	stop_readJSON_time = time.time()
+	dictionary_all.update({"Time to do shit with file": start_readJSON_time - start_file_time})
+	dictionary_all.update({"Time to read JSONFile": stop_readJSON_time - start_readJSON_time})
 	os.remove(tweetFile)
 	return dictionary_all
 
